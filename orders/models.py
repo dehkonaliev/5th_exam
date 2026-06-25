9d1ff1a5200335ebaefcf5435cadcc2e39f80e2c
Maqsudbek <dehkanaliev@gmail.com>
2026-06-25 21:07:24 +0500

    Products qismiga Product modelini qo'shdim! Orders appiga Saved, Order, OrderItem modellarini qo'shdim va migratsiya berdim

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from products.models import Product
+from users.models import CustomUser
 
-# Create your models here.
+class Saved(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name}"
+    
+
+class Order(models.Model):
+    STATUS = (
+        ('pending', 'Pending'),
+        ('shipped' ,'Shipped'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled')
+    )
+    
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price_at_purchase = models.DecimalField(max_digits=20, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.quantity} x {self.product} - {self.order.id}"
+    
+
+
+    
--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=500, null=True, blank=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    quantity = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
